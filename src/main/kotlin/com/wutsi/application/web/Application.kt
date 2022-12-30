5f8361800c7e4bb7416b65fbb3cd6bd5ab9e7a78
Herve Tchepannou <herve@wutsi.com>
2022-12-30 10:55:32 -0500

    add robots.txt support

--- src/main/kotlin/com/wutsi/application/web/Application.kt
+++ src/main/kotlin/com/wutsi/application/web/Application.kt
@@ -2,9 +2,11 @@ package com.wutsi.application.web
 
 import com.wutsi.platform.core.WutsiApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.web.servlet.ServletComponentScan
 
 @WutsiApplication
 @SpringBootApplication
+@ServletComponentScan
 class Application
 
 fun main(vararg args: String) {
